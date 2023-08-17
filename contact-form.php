cc0d8b05b1f5c4fb1c0afca145b41d5e0feed8b8
Odunayomike <micheal0olawale@gmail.com>
2023-08-17 12:18:51 +0100

    working on contact form backend

--- contact-form.php
+++ contact-form.php
@@ -1,5 +1,45 @@
 <?php
 
-echo "Hello there";
+if ($_SERVER["REQUEST_METHOD"] === "POST") {
+    // Sanitize and validate form data
+    $name = htmlspecialchars($_POST["name"], ENT_QUOTES, 'UTF-8');
+    $subject = htmlspecialchars($_POST["subject"], ENT_QUOTES, 'UTF-8');
+    $email = filter_var($_POST["email"], FILTER_SANITIZE_EMAIL);
+    $message = htmlspecialchars($_POST["message"], ENT_QUOTES, 'UTF-8');
+
+    // Validate email
+    if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+        echo "Invalid email format.";
+        exit();
+    }
+
+    // Create email content
+    $subject = "New Contact Form Submission";
+    $to = "info@deuselconsult.com";
+    $headers = "From: $email\r\n";
+    $headers .= "Reply-To: $email\r\n";
+    $headers .= "Content-Type: text/html; charset=UTF-8\r\n";
+    
+    $emailContent = "
+        <html>
+        <head>
+            <title>$subject</title>
+        </head>
+        <body>
+            <p><strong>Name:</strong> $name</p>
+            <p><strong>Email:</strong> $email</p>
+            <p><strong>Subject:</strong> $subject</p>
+            <p><strong>Message:</strong> $message</p>
+        </body>
+        </html>
+    ";
+
+    // Send email
+    if (mail($to, $subject, $emailContent, $headers)) {
+        echo "Email sent successfully!";
+    } else {
+        echo "Error sending email.";
+    }
+}
 
 ?>
